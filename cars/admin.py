7243679034eaeea3032dd9fb01806e0335900e1e
erobakiewicz <eliasz.robakiewicz@laboratorium.ee>
2022-01-24 21:01:53 +0100

    Car and rating models

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from cars.models import Car, CarRating
 
-# Register your models here.
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = [
+        'make',
+        'model',
+        'avg_rating'
+    ]
+
+
+@admin.register(CarRating)
+class CarRatingAdmin(admin.ModelAdmin):
+    pass
--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,33 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.db.models import Avg
+from django.contrib import admin
 
-# Create your models here.
+
+class Car(models.Model):
+    make = models.CharField("make", max_length=100)
+    model = models.CharField("model", max_length=100)
+
+    def __str__(self):
+        return f'{self.make}: {self.model}'
+
+    @property
+    @admin.display(description="average rating")
+    def avg_rating(self):
+        return Car.objects.aggregate(Avg('ratings__rating'))
+
+    class Meta:
+        verbose_name = "Car"
+        verbose_name_plural = "Cars"
+
+
+class CarRating(models.Model):
+    rating = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    rated_car = models.ForeignKey('Car', on_delete=models.CASCADE, verbose_name="rated car", related_name="ratings")
+
+    def __str__(self):
+        return f'{self.rated_car.model}: {self.rating}'
+
+    class Meta:
+        verbose_name = "Car rating"
+        verbose_name_plural = "Cars ratings"
